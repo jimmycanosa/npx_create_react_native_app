14e9bd12837d96d76a4e9e5f838a6cc4784438c0
jimmycanosa <jimmycanosajr@gmail.com>
2023-08-17 13:17:19 +0800

    modal.js tutorial

--- app/_layout.js
+++ app/_layout.js
@@ -1,8 +1,9 @@
-import { Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
-import { Alert } from 'react-native';
 
 export default () => {
+  const router = useRouter();
+  
   return (
     <Stack
       screenOptions={{
@@ -10,18 +11,19 @@ export default () => {
         // title: 'Title-Stack_layout',
         headerTintColor: 'white',
         headerTitleStyle: { fontWeight: 'bold' },
-        headerRight: () => (
-          <Ionicons
-          onPress={() => Alert.alert("More info")}
-            name='ios-information-circle-outline'
-            size={30}
-            color='black'
-          />
-        ),
+        // headerRight: () => (
+        //   <Ionicons
+        //   onPress={() => router.push('/modal')}
+        //     name='ios-information-circle-outline'
+        //     size={30}
+        //     color='black'
+        //   />
+        // ),
       }}
     >
       {/* do this if you want static layout and will be applied to all */}
       <Stack.Screen name='index' options={{ title: 'Home_layout.js' }} />
+      <Stack.Screen name='modal' options={{ presentation: 'modal' }} />
     </Stack>
   );
 };
--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-import { Link, Stack } from 'expo-router';
+import { Link, Stack, useRouter } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -8,16 +8,25 @@ const users = [
   { username: 'RLuad', name: 'Regine' },
 ];
 
+const router = useRouter();
+
 export default function Page() {
+  // const router = useRouter();
+  
   return (
     <View style={styles.container}>
       {/* do this for dynamic layout */}
-      {/* <Stack.Screen options={{ 
+      <Stack.Screen options={{ 
         title: 'Home-index.js',
         headerRight: () => (
-          <Ionicons name="ios-information-circle-outline" size={30} color="black" />
+          <Ionicons
+          onPress={() => router.push('/modal')}
+            name='ios-information-circle-outline'
+            size={30}
+            color='black'
+          />
         ),
-        }} /> */}
+        }} />
       <View style={styles.main}>
         <Text style={styles.title}>Hello World</Text>
         <Text style={styles.subtitle}>This is the first page of your app.</Text>
